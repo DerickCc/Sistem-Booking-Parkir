6f5e4ef08557761a741932574e2f0256e7bcefb9
DerickCc <derickchainatra@gmail.com>
2023-04-14 16:44:52 +0700

    f

--- parkir2/page-user/booking.php
+++ parkir2/page-user/booking.php
@@ -41,7 +41,7 @@
                 ?>
                 <div class="col-10 mx-auto px-0">
                     <div class='alert alert-danger alert-dismissible fade show' role='alert'>
-                        <i class='fa-solid fa-circle-xmark mr-2'></i> Tidak ada lokasi yang ditemukan.
+                        <i class='fa-solid fa-circle-xmark mr-2'></i> Lokasi tidak ditemukan.
                         <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                     </div>
                 </div>
